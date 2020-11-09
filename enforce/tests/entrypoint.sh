827283dbb786b6c3671433ff4bc8319c6332bfe2
monshah <monshah@splunk.com>
2020-11-09 14:20:01 +0530

    Updated-latest-requirements

--- enforce/tests/entrypoint.sh
+++ enforce/tests/entrypoint.sh
@@ -7,7 +7,7 @@
 
 cd /home/circleci/work
 if [ -f "${TEST_SET}/pytest-ci.ini" ]; then
-    cp -f ${TEST_SET}/pytest-ci.ini ${TEST_SET}/pytest.ini
+    cp -f ${TEST_SET}/pytest-ci.ini pytest.ini
 fi
 
 pip install -r ${TEST_SET}/requirements.txt
