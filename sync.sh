ced0bb1af2aa13d1b15ccada57d6193bcb2e2574
Jay Joshi <37693483+jayjoshi-crest@users.noreply.github.com>
2021-01-21 19:40:58 +0530

    test: fixing the new branch change

--- sync.sh
+++ sync.sh
@@ -120,6 +120,7 @@ do
         git config  user.name "Addon Factory template"
 
         echo "changing the defailt branch"
+        git fetch --all
         git checkout -b temp_main master || echo "main branch already exists"
         hub api repos/$REPOORG/$REPO -X PATCH -f name=$REPO -f default_branch=temp_main
 
