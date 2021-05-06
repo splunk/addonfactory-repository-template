7380d673a5299c16fd4675afc5b8b1e63014850a
seanmalloy-crest <smalloy@splunk.com>
2021-05-05 22:26:26 -0700

    test

--- sync.sh
+++ sync.sh
@@ -113,9 +113,9 @@ do
         hub api orgs/$REPOORG/teams/products-gdi-addons-adminrepo/repos/$REPOORG/$REPO --raw-field 'permission=admin' -X PUT
         echo "Adding branch protection"
         # hub api -X PATCH -H "Accept: application/vnd.github.v3+json" /repos/$REPOORG/$REPO/branches/main/protection --raw-field '{"dismissal_restrictions": {},"dismiss_stale_reviews": false,"require_code_owner_reviews": true,"required_approving_review_count": 1}'
-        # hub api -X PATCH -H "Accept: application/vnd.github.v3+json" /repos/splunk/addonfactory-repository-template/environments/test_environment --raw-field '{"reviewers": [{"type": "User", "id": 60528866}]}'
-        curl -X PATCH -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$REPOORG/$REPO/branches/main/protection -d '{"dismissal_restrictions": {},"dismiss_stale_reviews": false,"require_code_owner_reviews": true,"required_approving_review_count": 1}'
-        curl -X PUT -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/splunk/addonfactory-repository-template/environments/test_environment -d '{"reviewers": [{"type": "User", "id": 60528866}]}'
+        hub api -X PUT -H "Accept: application/vnd.github.v3+json" /repos/splunk/addonfactory-repository-template/environments/test_environment --raw-field '{"reviewers": [{"type": "User", "id": 60528866}]}'
+        curl -X PUT -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$REPOORG/$REPO/branches/main/protection -d '{"dismissal_restrictions": {},"dismiss_stale_reviews": false,"require_code_owner_reviews": true,"required_approving_review_count": 1}'
+        # curl -X PUT -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/splunk/addonfactory-repository-template/environments/test_environment -d '{"reviewers": [{"type": "User", "id": 60528866}]}'
         curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/splunk/addonfactory-repository-template/environments
         if [ ! -d "$REPO" ]; then
             #hub clone $REPOORG/$REPO work/$REPO
